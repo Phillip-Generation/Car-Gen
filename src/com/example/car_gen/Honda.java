fb9dc4945932fdd156c2cc1730dabe003ffd9935
Tyson Code Cademy <Tyson3rd@Michelles-MacBook-Air.local>
2021-06-09 15:54:57 -0500

    First Commit

---
+++ src/com/example/car_gen/Honda.java
@@ -0,0 +1,34 @@
+package com.example.car_gen;
+
+public class Honda extends Car{
+    private boolean isTurboCharged;
+    public Honda(String color, int year, int speed, int maxSpeed, boolean isElectric, boolean isTurboCharged){
+        super(color,year,speed,maxSpeed,isElectric);
+        this.isTurboCharged = isTurboCharged;
+    }
+
+    // click code, generate, getter and setter
+
+
+//    public boolean isTurboCharged() {
+//        return isTurboCharged;
+//    }
+//
+//    public void setTurboCharged(boolean turboCharged) {
+//        isTurboCharged = turboCharged;
+//    }
+
+    //Getter & Setter
+    public boolean getIsTurboCharged(){
+        return isTurboCharged;
+    }
+    public void setIsTurboCharged(boolean isTurboCharged){
+        this.isTurboCharged = isTurboCharged;
+    }
+
+    @Override
+    public String toString() {
+        return "Make:Honda Color: "+super.getColor()+" Year: "+super.getYear()+" Speed: "+super.getSpeed()+" Max Speed: "+"Electric: "+ super.getIsElectric()+" Turbo:"+ isTurboCharged;
+    }
+}
+
