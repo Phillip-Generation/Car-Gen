a766ec28a37fb9fa6715c5adc45b25116dba5ab4
Ashley U <ashley.umetsu@gmail.com>
2021-06-09 13:16:30 -0500

    adding getters and setters

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,51 @@
 public class Car {
-    String color;
-    int year;
-    int speed = 0;
-    int maxSpeed;
-    boolean isElectric;
+    private String color;
+    private int year;
+    private int speed = 0;
+    private int maxSpeed;
+    private boolean isElectric;
 
     public Car(String color, int year, int speed, int maxSpeed) {
 
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
     }
 }
